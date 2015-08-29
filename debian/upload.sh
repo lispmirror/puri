789e972d75dfe1e8432a11a8fa1b13b1b5ecb469
Kevin M. Rosenberg <kevin@rosenberg.net>
2015-08-29 00:16:20 -0600

    Update domain name to kpe.io

--- debian/upload.sh
+++ debian/upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-dup puri -Ufiles.b9.com -D/home/ftp/puri -C"(umask 022; /home/kevin/bin/remove-old-versions puri latest)" -su $*
+dup puri -Ufiles.kpe.io -D/home/ftp/puri -C"(umask 022; /home/kevin/bin/remove-old-versions puri latest)" -su $*
